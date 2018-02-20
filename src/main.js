185fd3826f806bf178e8f0161a750002f1b3a385
He Qingbao <qingbao.ho@gmail.com>
2018-02-20 16:30:46 +0800

    集成vue-router模块

--- src/main.js
+++ src/main.js
@@ -4,9 +4,24 @@ import Vue from 'vue';
 // 2.0 导入App.vue的vue对象
 import App from './App.vue';
 
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import login from './components/account/login.vue'
+import register from './components/account/register.vue'
+
+var router = new VueRouter({
+    routes: [
+        {path: '/login', component: login},
+        {path: '/register', component: register}
+    ]
+});
+
 // 3.0 利用Vue对象进行解析渲染
 new Vue({
-	el:'#app',
-	// render:function(create){create(App)} //es5的写法
-	render:c=>c(App)  // es6的函数写法 =>：goes to
+    el: '#app',
+    router: router,
+    // render:function(create){create(App)} //es5的写法
+    render: c => c(App)  // es6的函数写法 =>：goes to
 });
